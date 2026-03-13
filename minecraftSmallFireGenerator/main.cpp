fc4da2a2830eeef81d2a338c55b0987af0fed812
蒼一朗 石井 <h1eq1ta@gmail.com>
2026-03-13 22:47:36 +0900

    でけた～

--- minecraftSmallFireGenerator/main.cpp
+++ minecraftSmallFireGenerator/main.cpp
@@ -1,33 +1,59 @@
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #include<string>
+#include<vector>
+#define STBI_ONLY_PNG
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
-
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
 int main() {
 	//入力
-	std::string path = "";	//what
-	int fireSizeH = 0;
+	std::string path = "";
+	size_t afterH = 0;
 	std::cout << "変更を加える火のテクスチャのパスを入力して下さい:";
 	std::getline(std::cin, path);
-	std::cout << "火単体の解像度を入力してください:";
-	std::cin >> fireSizeH;
+	std::cout << "生成後の火単体の大きさを入力してください:";
+	std::cin >> afterH;
 	//読み込み
 	int w, h, c;
-	auto befFire = stbi_load(path.c_str(), &w, &h, &c, 4);
-	if (befFire == nullptr) {
-		std::cout << "テクスチャを読み込めませんでした\n";
-		system("pause>0");
+	auto inFire = stbi_load(path.c_str(), &w, &h, &c, 4);
+	size_t numDeleteH = w - afterH;
+	if (afterH > w) {
+		std::cout << "元画像の高さより高くなってどうすんねん。\n";
+		system("pause");
 		return -1;
 	}
-	stbi_image_free(befFire);
-}
+	if (inFire == nullptr) {
+		std::cout << "テクスチャを読み込めませんでした\n";
+		system("pause");
+		return -1;
+	}
+	if (h % w != 0) {
+		std::cout << "画像の高さと幅の値が不正です\n";
+		system("pause");
+		return -1;
+	}
+	int numFireFrames = h / w;
 
-//1 > main.cpp
-//1 > C:\Users\石井蒼一朗\source\repos\minecraftSmallFireGenerator\minecraftSmallFireGenerator\main.cpp(1, 1) : warning C4819 : ファイルは、現在のコード ページ(932) で表示できない文字を含んでいます。データの損失を防ぐために、ファイルを Unicode 形式で保存してください。
-//1 > C:\Users\石井蒼一朗\source\repos\minecraftSmallFireGenerator\minecraftSmallFireGenerator\main.cpp(14, 25) : error C2065 : 'path' : 定義されていない識別子です。
-//1 > C:\Users\石井蒼一朗\source\repos\minecraftSmallFireGenerator\minecraftSmallFireGenerator\main.cpp(19, 27) : error C2065 : 'path' : 定義されていない識別子です。
-//1 > C:\Users\石井蒼一朗\source\repos\minecraftSmallFireGenerator\minecraftSmallFireGenerator\main.cpp(20, 6) : error C3536 : 'befFire' : 初期化前に使用することはできません
+	std::vector<unsigned char> outFire;
+	outFire.resize(w * h * 4, 0);
+	//出力
+	size_t copySizePerFire = afterH * w * 4;
+	size_t deleteSizePerFire = numDeleteH * w * 4;
+	size_t sizePerFire = w * w * 4;
+	for (int i = 0; i < numFireFrames; i++) {
+		size_t nowFireHead = sizePerFire * i;
+		memcpy(&outFire[nowFireHead + deleteSizePerFire],
+			&inFire[nowFireHead],
+			copySizePerFire);
+	}
+	std::cout << "処理が終了しました。\n";
+	std::string outFileName = "out ";
+	srand(time(NULL));
+	outFileName += std::to_string(rand());
+	outFileName += ".png";
+	stbi_write_png(outFileName.c_str(), w, h, c, outFire.data(), w * 4);
+	stbi_image_free(inFire);
+}
